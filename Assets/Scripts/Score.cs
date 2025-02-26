ea0741116b30677facd205ff411dae70e6479e3d
JaviMontes <fmontesestrabon@safareyes.es>
2025-02-26 17:49:28 +0100

    Añadido contador de monedas

---
+++ Assets/Scripts/Score.cs
@@ -0,0 +1,25 @@
+﻿using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Score : MonoBehaviour
+{
+    public static int score;
+    public TextMeshProUGUI scoreText;
+
+    void Start()
+    {
+        score = 0;
+        scoreText.text = score + "/10";
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if(collision.gameObject.tag == "Moneda")
+        {
+            score++;
+            Debug.Log(score);
+            scoreText.text = score +"/10";
+        }
+    }
+}
